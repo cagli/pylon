6680d23d9fea573599cd6470d690e5b95cf3a0cb
cagli <cagli@me.com>
2015-07-06 16:00:47 +0100

    change attribute due to this http://stackoverflow.com/questions/11534902/ruby-on-rails-app-works-locally-but-does-not-work-on-heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+
+	add :remember_token to attr_accessible
+
+	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
