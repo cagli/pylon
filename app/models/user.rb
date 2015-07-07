e2796dbc91e7ce4ea5e7c3bf8b609d39ddab4e3e
cagli <cagli@me.com>
2015-07-07 12:17:16 +0100

    syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-	add :remember_token to attr_accessible
 
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
